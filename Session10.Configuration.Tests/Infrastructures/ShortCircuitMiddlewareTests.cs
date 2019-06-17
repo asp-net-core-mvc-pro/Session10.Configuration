c47fe41ad4ac6247f0a846d002c97491cd247058
morsal mousavi <morsal84@gmail.com>
2019-06-17 09:04:34 +0430

    Add project files.

---
+++ Session10.Configuration.Tests/Infrastructures/ShortCircuitMiddlewareTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Session10.Configuration.Infrastructures;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Session10.Configuration.Tests.Infrastructures
+{
+    public class ShortCircuitMiddlewareTests
+    {
+        private readonly Task _onNextResult = Task.FromResult(0);
+        private readonly RequestDelegate _onNext;
+        private readonly DefaultHttpContext _context;
+
+        public ShortCircuitMiddlewareTests()
+        {
+            _onNext = _ =>
+            {
+                return _onNextResult;
+            };
+            _context = new DefaultHttpContext();
+        }
+
+        [Fact]
+        public async Task WhenBrowserIsEdgeThenStatusCode403()
+        {
+            // arrange
+            var secureHeadersMiddleware = new ShortCircuitMiddleware(_onNext);
+            _context.Items.Add("EdgeBrowser", "true");
+
+            // act
+            await secureHeadersMiddleware.Invoke(_context);
+
+            Assert.True(_context.Response.StatusCode == 403);
+        }
+    }
+}
